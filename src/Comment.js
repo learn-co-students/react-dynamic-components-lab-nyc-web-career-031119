bfa3447b7f2c1a7d41ff42693c0c24438952d895
Locallize <Fillanlee@aol.com>
2019-05-14 14:17:00 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
 // add Comment component here
+import React, { Component } from 'react';
+
+export default class Comment extends React.Component{
+    render(){
+       return (
+        //    React.createElement(div,{}, nil)
+        <div className="comment">'poop'</div>
+           )
+    }
+}
+// export default Comment
